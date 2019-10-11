03329f1d8466f8044ea53c47417251a433867403
Pham Trung Chinh <chinhph97@gmail.com>
2019-10-11 17:53:53 +0700

    add form cameraImge

--- camerawebapp/src/App.js
+++ camerawebapp/src/App.js
@@ -2,11 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Login from './Component/Login/Login.js'
-
+import Camera from './Component/Camera/Camera.js'
 function App() {
   return (
-     <Login/>
-  
+    <div className="App">
+      <Login/>
+      <Camera/>
+    </div>
+
   );
 }
 
